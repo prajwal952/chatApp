17c319634200aecd6a99fdc5144cb5ad51070027
prajwal952 <prajwalmpatil952@gmai.com>
2024-11-27 06:48:47 +0530

    right bar and menu hover ui

--- src/Components/RightSideBar/RightSideBar.jsx
+++ src/Components/RightSideBar/RightSideBar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 
 const RightSideBar = () => {
   return (
-    <div className='rf'>RightSideBar</div>
+    <div className='rs'>
+        <div className="rs-profile">
+            <img src={assets.profile_img} alt="" />
+            <h3>Rakesh Modi
+                <img src={assets.green_dot}  alt="" className='dot'/>
+            </h3>
+            <p>Hey, There i am Rakesk using chat app</p>
+        </div>
+        <hr />
+        <div className="rs-media">
+            <p>Media</p>
+            <div>
+                <img src={assets.pic1} alt="" />
+                <img src={assets.pic2} alt="" />
+                <img src={assets.pic3} alt="" />
+                <img src={assets.pic4} alt="" />
+                <img src={assets.pic1} alt="" />
+                <img src={assets.pic2} alt="" />
+            </div>
+            <button>Log Out</button>
+        </div>
+    </div>
   )
 }
 
