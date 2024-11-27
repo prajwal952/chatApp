179c53d5c3f713db54a35882a3b4f16a37f944d6
prajwal952 <prajwalmpatil952@gmai.com>
2024-11-27 08:08:22 +0530

    pofile page ui done

--- src/Pages/ProfileUpdate/ProfileUPdate.jsx
+++ src/Pages/ProfileUpdate/ProfileUPdate.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './PorfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUPdate = () => {
   return (
-    <div>ProfileUPdate</div>
+    <div className='profile'>
+        <div className="profile-container">
+            <form >
+                <h3>Profile Details</h3>
+                <label htmlFor="avatar">
+                    <input type="file" id='avatar' accept='.png, .jpg, .jpeg ' hidden/>
+                    <img src={assets.avatar_icon} alt="" />
+                    upload profile image
+                </label>
+
+                <input type="text" placeholder='your name' required name="" id="" />
+                <textarea placeholder='write profile bio' required></textarea>
+                <button type='submit'>Save</button>
+
+            </form>
+            <img className='profile-pic' src={assets.logo_icon} alt="" />
+        </div>
+    </div>
   )
 }
 
